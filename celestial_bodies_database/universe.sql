a0e07839f2f75b0bf7e5f4399d5586ad33d70de4
moT01 <20648924+moT01@users.noreply.github.com>
2021-06-03 09:44:01 -0500

    update solution

--- celestial_bodies_database/universe.sql
+++ celestial_bodies_database/universe.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.6.17
--- Dumped by pg_dump version 9.6.17
+-- Dumped from database version 12.6 (Debian 12.6-1.pgdg90+1)
+-- Dumped by pg_dump version 12.6 (Debian 12.6-1.pgdg90+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -16,23 +16,32 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+DROP DATABASE universe;
 --
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+-- Name: universe; Type: DATABASE; Schema: -; Owner: freecodecamp
 --
 
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+CREATE DATABASE universe WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
 
+
+ALTER DATABASE universe OWNER TO freecodecamp;
+
+\connect universe
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
 
 SET default_tablespace = '';
 
-SET default_with_oids = false;
+SET default_table_access_method = heap;
 
 --
 -- Name: galaxy; Type: TABLE; Schema: public; Owner: freecodecamp
@@ -182,7 +191,7 @@ ALTER SEQUENCE public.planet_planet_id_seq OWNED BY public.planet.planet_id;
 
 CREATE TABLE public.planet_types (
     planet_types_id integer NOT NULL,
-    name character varying(60),
+    name character varying(60) NOT NULL,
     description text
 );
 
@@ -292,14 +301,87 @@ ALTER TABLE ONLY public.star ALTER COLUMN star_id SET DEFAULT nextval('public.st
 -- Data for Name: galaxy; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-COPY public.galaxy (galaxy_id, name, ly_width, ly_height, galaxy_types_id) FROM stdin;
-1	Andromeda	220000	\N	1
-2	Milky Way	170000	2000	1
-3	Antennae	500000	\N	3
-4	Sombrero	50000	\N	1
-5	Large Magellanic Cloud	14000	\N	3
-6	Small Magellanic Cloud	7000	\N	3
-\.
+INSERT INTO public.galaxy VALUES (1, 'Andromeda', 220000, NULL, 1);
+INSERT INTO public.galaxy VALUES (2, 'Milky Way', 170000, 2000, 1);
+INSERT INTO public.galaxy VALUES (3, 'Antennae', 500000, NULL, 3);
+INSERT INTO public.galaxy VALUES (4, 'Sombrero', 50000, NULL, 1);
+INSERT INTO public.galaxy VALUES (5, 'Large Magellanic Cloud', 14000, NULL, 3);
+INSERT INTO public.galaxy VALUES (6, 'Small Magellanic Cloud', 7000, NULL, 3);
+
+
+--
+-- Data for Name: galaxy_types; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.galaxy_types VALUES (1, 'Sprial', 'Galaxy that has a spiral shape.');
+INSERT INTO public.galaxy_types VALUES (2, 'Elliptical', 'This type of galaxy has a spherical shape.');
+INSERT INTO public.galaxy_types VALUES (3, 'Irregular', 'This type has an irregular shape that does not fit into one of the other categories.');
+
+
+--
+-- Data for Name: moon; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.moon VALUES (1, 'The Moon', 3, false, true);
+INSERT INTO public.moon VALUES (2, 'Phobos', 4, false, false);
+INSERT INTO public.moon VALUES (3, 'Demos', 4, false, false);
+INSERT INTO public.moon VALUES (4, 'Io', 5, false, true);
+INSERT INTO public.moon VALUES (5, 'Europa', 5, false, true);
+INSERT INTO public.moon VALUES (6, 'Ganymede', 5, false, true);
+INSERT INTO public.moon VALUES (7, 'Callisto', 5, false, true);
+INSERT INTO public.moon VALUES (8, 'Titan', 6, true, true);
+INSERT INTO public.moon VALUES (9, 'Mimas', 6, false, true);
+INSERT INTO public.moon VALUES (10, 'Epimetheus', 6, false, false);
+INSERT INTO public.moon VALUES (11, 'Janus', 6, false, false);
+INSERT INTO public.moon VALUES (12, 'Pandora', 6, false, false);
+INSERT INTO public.moon VALUES (13, 'Prometheus', 6, false, false);
+INSERT INTO public.moon VALUES (14, 'Puck', 7, false, true);
+INSERT INTO public.moon VALUES (15, 'Miranda', 7, false, true);
+INSERT INTO public.moon VALUES (16, 'Ariel', 7, false, true);
+INSERT INTO public.moon VALUES (17, 'Ubriel', 7, false, true);
+INSERT INTO public.moon VALUES (18, 'Titania', 7, false, true);
+INSERT INTO public.moon VALUES (19, 'Oberon', 7, false, true);
+INSERT INTO public.moon VALUES (20, 'Triton', 8, false, true);
+
+
+--
+-- Data for Name: planet; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.planet VALUES (1, 'Mercury', 1, 1, false);
+INSERT INTO public.planet VALUES (2, 'Venus', 1, 1, false);
+INSERT INTO public.planet VALUES (3, 'Earth', 1, 1, true);
+INSERT INTO public.planet VALUES (7, 'Neptune', 1, 2, false);
+INSERT INTO public.planet VALUES (8, 'Uranus', 1, 2, false);
+INSERT INTO public.planet VALUES (9, 'Pluto', 1, 3, false);
+INSERT INTO public.planet VALUES (11, 'Proxima Centauri a', 2, 2, false);
+INSERT INTO public.planet VALUES (12, 'Proxima Centauri c', 2, 1, true);
+INSERT INTO public.planet VALUES (5, 'Jupiter', 1, 2, false);
+INSERT INTO public.planet VALUES (6, 'Saturn', 1, 2, false);
+INSERT INTO public.planet VALUES (10, 'Proxima Centauri b', 2, 2, false);
+INSERT INTO public.planet VALUES (4, 'Mars', 1, 1, false);
+
+
+--
+-- Data for Name: planet_types; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.planet_types VALUES (1, 'Terrestrial', 'Primarily comprised of a rocky type surface.');
+INSERT INTO public.planet_types VALUES (2, 'Gas Giant', 'Massive planets primarily comprised of gas in place of a solid surface.');
+INSERT INTO public.planet_types VALUES (3, 'Ice Giants', 'Massive planets with a solid ice surface.');
+INSERT INTO public.planet_types VALUES (4, 'Dwarf Planets', 'Smaller than terrestrial planets but share many of their characteristics');
+
+
+--
+-- Data for Name: star; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.star VALUES (1, 'Sun', 2, 4.6, 0.0);
+INSERT INTO public.star VALUES (2, 'Proximy Centauri', 2, 4.8, 4.2);
+INSERT INTO public.star VALUES (3, 'Bernards Star', 2, 10.0, 6.0);
+INSERT INTO public.star VALUES (4, 'Sirius', 2, 0.2, 8.6);
+INSERT INTO public.star VALUES (5, '61 Cygni A', 2, 6.1, 11.4);
+INSERT INTO public.star VALUES (6, '61 Cygni B', 2, 6.1, 11.4);
 
 
 --
@@ -310,49 +392,10 @@ SELECT pg_catalog.setval('public.galaxy_galaxy_id_seq', 6, true);
 
 
 --
--- Data for Name: galaxy_types; Type: TABLE DATA; Schema: public; Owner: freecodecamp
---
-
-COPY public.galaxy_types (galaxy_types_id, name, description) FROM stdin;
-1	Sprial	Galaxy that has a spiral shape.
-2	Elliptical	This type of galaxy has a spherical shape.
-3	Irregular	This type has an irregular shape that does not fit into one of the other categories.
-\.
-
-
---
 -- Name: galaxy_types_galaxy_types_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
 SELECT pg_catalog.setval('public.galaxy_types_galaxy_types_id_seq', 3, true);
-
-
---
--- Data for Name: moon; Type: TABLE DATA; Schema: public; Owner: freecodecamp
---
-
-COPY public.moon (moon_id, name, planet_id, has_life, spherical) FROM stdin;
-1	The Moon	3	f	t
-2	Phobos	4	f	f
-3	Demos	4	f	f
-4	Io	5	f	t
-5	Europa	5	f	t
-6	Ganymede	5	f	t
-7	Callisto	5	f	t
-8	Titan	6	t	t
-9	Mimas	6	f	t
-10	Epimetheus	6	f	f
-11	Janus	6	f	f
-12	Pandora	6	f	f
-13	Prometheus	6	f	f
-14	Puck	7	f	t
-15	Miranda	7	f	t
-16	Ariel	7	f	t
-17	Ubriel	7	f	t
-18	Titania	7	f	t
-19	Oberon	7	f	t
-20	Triton	8	f	t
-\.
 
 
 --
@@ -363,26 +406,6 @@ SELECT pg_catalog.setval('public.moon_moon_id_seq', 20, true);
 
 
 --
--- Data for Name: planet; Type: TABLE DATA; Schema: public; Owner: freecodecamp
---
-
-COPY public.planet (planet_id, name, star_id, planet_types_id, has_life) FROM stdin;
-1	Mercury	1	1	f
-2	Venus	1	1	f
-3	Earth	1	1	t
-7	Neptune	1	2	f
-8	Uranus	1	2	f
-9	Pluto	1	3	f
-11	Proxima Centauri a	2	2	f
-12	Proxima Centauri c	2	1	t
-5	Jupiter	1	2	f
-6	Saturn	1	2	f
-10	Proxima Centauri b	2	2	f
-4	Mars	1	1	f
-\.
-
-
---
 -- Name: planet_planet_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
@@ -390,36 +413,10 @@ SELECT pg_catalog.setval('public.planet_planet_id_seq', 12, true);
 
 
 --
--- Data for Name: planet_types; Type: TABLE DATA; Schema: public; Owner: freecodecamp
---
-
-COPY public.planet_types (planet_types_id, name, description) FROM stdin;
-1	Terrestrial	Primarily comprised of a rocky type surface.
-2	Gas Giant	Massive planets primarily comprised of gas in place of a solid surface.
-3	Ice Giants	Massive planets with a solid ice surface.
-4	Dwarf Planets	Smaller than terrestrial planets but share many of their characteristics
-\.
-
-
---
 -- Name: planet_types_planet_types_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
 SELECT pg_catalog.setval('public.planet_types_planet_types_id_seq', 4, true);
-
-
---
--- Data for Name: star; Type: TABLE DATA; Schema: public; Owner: freecodecamp
---
-
-COPY public.star (star_id, name, galaxy_id, age, distance) FROM stdin;
-1	Sun	2	4.6	0.0
-2	Proximy Centauri	2	4.8	4.2
-3	Bernards Star	2	10.0	6.0
-4	Sirius	2	0.2	8.6
-5	61 Cygni A	2	6.1	11.4
-6	61 Cygni B	2	6.1	11.4
-\.
 
 
 --
